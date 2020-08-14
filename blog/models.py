1a13a791264420a4f91e5f4a45fcb3f98036b2a7
anke <im.ak@outlook.com>
2020-08-14 10:39:34 +0800

    初始化项目

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from django.db import models
+from mdeditor.fields import MDTextField
+# Create your models here.
+
+
+class BlogType(models.Model):
+	type_name = models.CharField(max_length=15)
+
+	def __str__(self):
+		return self.type_name
+
+class Blog(models.Model):
+	title = models.CharField(max_length=50)
+	blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING)
+	content = MDTextField()
+	author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	created_time = models.DateTimeField(auto_now_add=True)
+	last_update_time = models.DateTimeField(auto_now=True)
+
+	def	__str__(self):
+		return "<Blog:%s>" %self.title
+
+	class Meta:
+		ordering = ['-created_time']
+
